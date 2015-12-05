e7b09045b045a20498b3faf9d6e17ceca1f039e6
jayne.hsu <jayne.hsu@ticketmaster.com>
2015-12-04 22:20:16 -0800

    change users to user

--- flywayFiles/sql/V1__build_from_existing_schema.sql
+++ flywayFiles/sql/V1__build_from_existing_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `emailAddress` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
